547cfd3282e173237431f464e7ef89de32d0721f
Radha Kunnattur <radzram@gmail.com>
2022-10-13 21:56:02 +0530

    Time: 248 ms (30.21%), Space: 71.7 MB (20.41%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,42 +1,30 @@
-class Solution {
-   public String longestPalindrome(String s) {
-	
-        boolean [][] dp = new boolean [s.length()][s.length()];
-	    String ans="";
-        String temp= "";
-		
-        for(int gap=0;gap<s.length();gap++){
-            for(int i=0,j=gap; j<dp.length;i++,j++){
-			
-                if(gap==0){
-                    dp[i][j]=true;
-                }
-                else if(gap==1){
-                if(s.charAt(i)==s.charAt(j)){
-                    dp[i][j]=true;
-                }
-                else{
-                    dp[i][j]=false;
-                }
-                }
-                else{
-                    if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==true){
-                        dp[i][j]=true;
-                    }
-                    else{
-                        dp[i][j]=false;
-                    }
-                }
-                if(dp[i][j]) {
-					 temp = s.substring(i, j+1);
-					 if(ans.length() < temp.length()) {
-						 ans = temp;
-                     }
+
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        String res = null;
+        int palindromeStartsAt = 0, maxLen = 0;
+
+        boolean[][] dp = new boolean[n][n];
+        // dp[i][j] indicates whether substring s starting at index i and ending at j is palindrome
+        
+        for(int i = n-1; i >= 0; i--) { // keep increasing the possible palindrome string
+            for(int j = i; j < n; j++) { // find the max palindrome within this window of (i,j)
                 
+                //check if substring between (i,j) is palindrome
+                dp[i][j] = (s.charAt(i) == s.charAt(j)) // chars at i and j should match
+                           && 
+                           ( j-i < 3  // if window is less than or equal to 3, just end chars should match
+                             || dp[i+1][j-1]  ); // if window is > 3, substring (i+1, j-1) should be palindrome too
+                
+                //update max palindrome string
+                if(dp[i][j] && (j-i+1 > maxLen)) {
+                    palindromeStartsAt = i;
+                    maxLen = j-i+1;
                 }
             }
         }
-        return ans;
+        return s.substring(palindromeStartsAt, palindromeStartsAt+maxLen);
     }
 }
-
