81a7358d2c92435506d68d3dc888d8c93217fb6a
Radha Kunnattur <radzram@gmail.com>
2022-10-30 17:04:27 +0530

    Time: 40 ms (46.59%), Space: 47.3 MB (75.95%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,36 +1,33 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int s_len = s.length(), p_len = p.length(), match = 0;
         List<Integer> anagrams = new ArrayList<>();
-        if(p_len > s_len) return anagrams;
-        
-        final int CHAR_SIZE = 26;
-        int[][] perm = new int[2][CHAR_SIZE]; // s - 0, p - 1
-        
-        for(int i = 0; i < p_len; i++) {
-            perm[0][s.charAt(i) - 'a']++;
-            perm[1][p.charAt(i) - 'a']++;
+        int n = s.length(), m=p.length();
+        int[][] hash = new int[2][26]; // p -> 0  s -> 1
+        if(m > n) return anagrams;
+        for(int i=0; i<m; i++){
+            int c1 = p.charAt(i) - 'a';
+            int c2 = s.charAt(i) - 'a';
+            hash[0][c1] += 1;
+            hash[1][c2] += 1;            
         }
-        
-        for(int i = 0; i < CHAR_SIZE; i++) {
-            if(perm[0][i] == perm[1][i]) match++;
+        int match = 0;
+        for(int i=0; i<26; i++){
+            if(hash[0][i] == hash[1][i]) match += 1;
         }
-        
-        if(match == CHAR_SIZE) anagrams.add(0);
-        for(int i = p_len; i < s_len; i++) { 
+        if(match == 26) anagrams.add(0);
+        for(int i=m; i<n; i++){
+            int cs = s.charAt(i) - 'a';
+            hash[1][cs] += 1;
+            if(hash[1][cs] == hash[0][cs]) match += 1;
+            if(hash[1][cs]-1 == hash[0][cs]) match -= 1;
             
-            int l = s.charAt(i - p_len) - 'a', r = s.charAt(i)-'a';
+            int ce = s.charAt(i-m) - 'a';
+            hash[1][ce] -= 1;
+            if(hash[1][ce] == hash[0][ce]) match += 1;
+            if(hash[1][ce]+1 == hash[0][ce]) match -= 1;
             
-            perm[0][l]--;
-            if(perm[0][l] == perm[1][l]) match++;
-            if(perm[0][l]+1 == perm[1][l]) match--;
-            
-            perm[0][r]++;
-            if(perm[0][r] == perm[1][r]) match++;
-            if(perm[0][r]-1 == perm[1][r]) match--;
-            if(match == CHAR_SIZE) anagrams.add(i-p_len+1);
+            if(match == 26) anagrams.add(i-m+1);
         }
-        
         return anagrams;
     }
 }
